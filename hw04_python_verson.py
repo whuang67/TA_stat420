12115aea55a2ae3d9912ef60bf70ae7205052c8f
Wenke Huang <whuang67@illinois.edu>
2017-07-05 00:02:35 -0500

    Update hw04_python_verson.py

--- hw04_python_verson.py
+++ hw04_python_verson.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Jul 01 21:36:06 2017
-
+Created on Sat Jul 05 00:01:06 2017
 @author: Wenke Huang
 """
 
@@ -10,6 +9,7 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 import scipy.stats as ss
 import matplotlib.pyplot as plt
+# from IPython.display import display
 
 def ANOVA(model, X, y):
     SSR = ((model.predict(X)-y.mean())**2).sum()
@@ -47,8 +47,51 @@ def Summary(model, X, y):
                            "Std_Error":se_beta,
                            "t_value": t_stat,
                            "p_value": p_of_t},
-                          index = np.append(X.columns, "Intercept"))
+                          index = np.append(X.columns, "(Intercept)"))
     return(result)
+
+def confint(model, X, y, level=0.95):
+    se = (((y-model.predict(X))**2).sum()/(X.shape[0]-X.shape[1]-1))**.5
+    X_1 = X.copy()
+    X_1['Intercept'] = pd.Series(np.array([1]*(X_1.shape[0]+1)))
+    C_diag = np.diag(np.linalg.inv(np.dot(np.transpose(X_1), X_1)))
+    coefficients = np.append(model.coef_, model.intercept_)
+    se_beta = se*C_diag**.5
+    crit_val = ss.t.ppf(.5+level/2, X.shape[0]-X.shape[1]-1)
+    lower = coefficients-crit_val*se_beta
+    upper = coefficients+crit_val*se_beta
+    result = pd.DataFrame({"Coefficients": coefficients,
+                           "Lower_bound": lower,
+                           "Upper_bound": upper},
+                          index = np.append(X.columns, "(Intercept)"))
+    return(result)
+
+def CI_y(model, X, y, x, level = 0.95, interval = "confidence"):
+    X_1 = X.copy()
+    X_1['Intercept'] = pd.Series(np.array([1]*(X_1.shape[0]+1)))
+    x_1 = x.copy()
+    x_1['Intercpet'] = pd.Series(np.array([1]))
+    se = (((y-model.predict(X))**2).sum()/(X.shape[0]-X.shape[1]-1))**.5
+    if interval == "confidence":
+        se_y = np.dot(np.dot(x_1,
+                             np.linalg.inv(np.dot(np.transpose(X_1),
+                                                  X_1))),
+                      np.transpose(x_1))**.5*se
+    elif interval == "prediction":
+        se_y = (np.dot(np.dot(x_1,
+                              np.linalg.inv(np.dot(np.transpose(X_1),
+                                                   X_1))),
+                       np.transpose(x_1))+1)**.5*se
+    c_val = ss.t.ppf(.5+level/2, X.shape[0]-X.shape[1])
+    y_hat = model.predict(x)
+    lower = y_hat-c_val*se_y
+    upper = y_hat+c_val*se_y
+    result = pd.DataFrame({"fit": y_hat,
+                           "lower": lower[0],
+                           "upper": upper[0]})
+    return result
+
+
 
 
 # Question 1
@@ -61,17 +104,20 @@ y_1 = nutrition.Calories
 regr_1a = LinearRegression().fit(X_1a, y_1)
 result_1a = ANOVA(regr_1a, X_1a, y_1)
 print "F-stat is {}.".format(result_1a['F_stat'])
-print "P-value is {}.".format(result_1a['p_value'])
+print "P-value is {:.4f}.".format(result_1a['p_value'])
     
 ## (b)
+print(Summary(regr_1a, X_1a, y_1))
+
 ## (c)
 X_1c = nutrition[['Carbs', 'Sodium', 'Fat', 'Protein']]
 regr_1c = LinearRegression().fit(X_1c, y_1)
 result_1c = ANOVA(regr_1c, X_1c, y_1)
 print "F-stat is {}.".format(result_1c['F_stat'])
-print "P-value is {}.".format(result_1c['p_value'])
+print "P-value is {:.4f}.".format(result_1c['p_value'])
 
 ## (d)
+print(Summary(regr_1c, X_1c, y_1))
 ## (e)
 
 
@@ -86,27 +132,37 @@ print "F-stat is {}.".format(result_2a["F_stat"])
 print "P-value is {}.".format(result_2a["p_value"])
 
 ## (b)
-print(regr_2a.coef_)
-print(regr_2a.intercept_)
+print Summary(regr_2a, X_2a, y_1)[['Coefficients']]
 ## (c)
-regr_2a.predict(pd.DataFrame({'Carbs': [47],
-                              'Fat': [28],
-                              'Protein': [25]}))
+print regr_2a.predict(pd.DataFrame({'Carbs': [47],
+                                    'Fat': [28],
+                                    'Protein': [25]}))
+    
 ## (d)
-regr_2a.predict(pd.DataFrame({'Carbs': [47],
-                              'Fat': [28],
-                              'Protein': [25]}))
-## (e)
 s_y = y_1.std()
 s_e = (((y_1-regr_2a.predict(X_2a))**2).sum()/(X_2a.shape[0]-X_2a.shape[1]-1))**.5
 print "s_y is {}.".format(s_y)
 print "s_e is {}.".format(s_e)
 
-## (f)
+## (e)
 print "R^2 is {}.".format(regr_2a.score(X_2a, y_1))
 
-## (g)
+## (f)
+print confint(regr_2a, X_2a, y_1, level = .9)
 
+## (g)
+print confint(regr_2a, X_2a, y_1)
+
+## (h)
+print CI_y(regr_2a, X_2a, y_1, pd.DataFrame({'Carbs': [30],
+                                             'Fat': [11],
+                                             'Protein': [2]}), level = 0.99)
+
+## (i)
+print CI_y(regr_2a, X_2a, y_1, pd.DataFrame({'Carbs': [11],
+                                             'Fat': [1.5],
+                                             'Protein': [1]}), level = 0.9,
+           interval = "prediction")
 
 
 
@@ -120,7 +176,7 @@ y_3 = goalies.W
 regr3_full = LinearRegression().fit(X_3a, y_3)
 result_3a = ANOVA(regr3_full, X_3a, y_3)
 print "F-stat is {}.".format(result_3a['F_stat'])
-print "P-value is {}.".format(result_3a['p_value'])
+print "P-value is {:.4f}.".format(result_3a['p_value'])
 
 ## (b)
 RMSE_3b = get_RMSE(regr3_full, X_3a, y_3)
@@ -140,6 +196,14 @@ print "RMSE of small model is {}.".format(RMSE_3d)
 
 ## (e)
 ## (f)
+SSE_reduced = ((y_3-regr3_small2.predict(X_3d))**2).sum()
+SSE_full = ((y_3-regr3_small.predict(X_3c))**2).sum()
+denominator = SSE_full/(X_3c.shape[0]-X_3c.shape[1]-1)
+numerator = (SSE_reduced-SSE_full)/(X_3c.shape[1]-X_3d.shape[1])
+F_stat = numerator/denominator
+p_value = ss.f.sf(F_stat, X_3c.shape[1]-X_3d.shape[1], X_3c.shape[0]-X_3c.shape[1]-1)
+print "F statistic is {}.".format(F_stat)
+print "p value is {:.4f}.".format(p_value)
 
 
 
@@ -161,8 +225,8 @@ ex_4_data = pd.DataFrame({"y": y,
                           "x2": x2,
                           "x3": x3,
                           "x4": x4})
-print(np.diag(C))
-print(ex_4_data[9:10])
+print np.diag(C)
+print ex_4_data[9:10]
 
 ## (b)
 beta_hat_1 = []
@@ -176,12 +240,14 @@ for i in range(0, 1500):
     y = 2+3*X.x1+4*X.x2+X.x4+np.random.normal(0, 4, n)
     regr = LinearRegression().fit(X, y)
     beta_hat_1.append(regr.coef_[0])
-    
+    p_values = Summary(regr, X, y).p_value.values
+    beta_2_pval.append(p_values[1:2][0])
+    beta_3_pval.append(p_values[2:3][0])
+
 ## (d)
+## (e)
 print np.mean(np.array(beta_hat_1))
 print np.std(np.array(beta_hat_1), ddof=1)
-
-## (e)
 plt.hist(beta_hat_1, 25, normed=True)
 fit_normal = ss.norm.pdf(sorted(beta_hat_1),
                          3,
@@ -190,4 +256,6 @@ plt.plot(sorted(beta_hat_1), fit_normal)
 plt.show()
 
 ## (f)
+print np.mean(np.array(beta_3_pval) < 0.05)
 ## (g)
+print np.mean(np.array(beta_2_pval) < 0.05)
