442ee7b0c254529c19970867f2bbdcc54d49a59a
Wenke Huang <whuang67@illinois.edu>
2017-08-02 09:49:44 -0500

    Update hw07_python_version.py

--- hw07_python_version.py
+++ hw07_python_version.py
@@ -2,17 +2,21 @@
 """
 Created on Sat Jul 29 10:46:40 2017
 
-@author: whuang67
+@author: Wenke Huang
 """
+
 import os
 import pandas as pd
 import numpy as np
+import math
 from sklearn.linear_model import LinearRegression
 # from sklearn.preprocessing import StandardScaler
 from patsy import dmatrices
 from statsmodels.stats.outliers_influence import variance_inflation_factor
 import scipy.stats as ss
 import matplotlib.pyplot as plt
+
+
 # Question 1
 ## (a)
 os.chdir("C:/Users/whuang67/downloads")
@@ -96,7 +100,7 @@ Normal_QQ(model_1e, X_1e, y_1)
 
 # Question 2
 ## (a)
-odor = pd.read_csv("odor.csv", index_col = 0)
+odor = pd.read_csv("odor.csv")
 y_2, X_2a = dmatrices('odor ~ (temp+gas+pack)**2+I(temp**2)+I(gas**2)+I(pack**2)',
                       odor,
                       return_type = 'dataframe')
@@ -123,8 +127,7 @@ print(ANOVA_1(model_2a, X_2a, y_2))
 _, X_2b = dmatrices('odor ~ temp+gas+pack+I(temp**2)+I(gas**2)+I(pack**2)',
                     odor,
                     return_type = 'dataframe')
-model_2b = LinearRegression(fit_intercept=0).fit(X_2b, y_2)
-print(model_2b.coef_[0])
+model_2b = LinearRegression(fit_intercept=False).fit(X_2b, y_2)
 print(ANOVA(model_2b, model_2a, X_2b, X_2a, y_2))
 
 ## (c)
@@ -144,3 +147,168 @@ print(adjusted_R_squared(model_2b, X_2b, y_2))
 # Question 3
 ## (a)
 teengamb = pd.read_csv('teengamb.csv')
+def get_AIC(model, X, y):
+    n = X.shape[0]
+    p = X.shape[1]
+    pred = model.predict(X)
+    RSS = ((y - pred)**2).sum()[0]
+    AIC = n*math.log(RSS/n) + 2*p
+    return AIC
+def get_BIC(model, X, y):
+    n = X.shape[0]
+    p = X.shape[1]
+    pred = model.predict(X)
+    RSS = ((y - pred)**2).sum()[0]
+    BIC = n*math.log(RSS/n) + math.log(n)*p
+    return BIC
+
+def backward(data, response, use="AIC", trace=True):
+    columns = data.columns.tolist()
+    columns.remove(response)
+    metric = {}
+    y_, X_ = data[response].to_frame(name = response), data.drop(response, axis=1)
+    X_['Intercept'] = [1]*X_.shape[0]
+    model = LinearRegression(fit_intercept=False).fit(X_, y_)
+    if use == "AIC":
+        metric['full'] = get_AIC(model, X_, y_)
+    elif use == "BIC":
+        metric['full'] = get_BIC(model, X_, y_)
+    for var in columns:
+        full_name = ""
+        for e in columns:
+            if e != var:
+                if full_name != "":
+                    full_name = full_name+"+"+e
+                else:
+                    full_name = response+str('~')+e
+        y_, X_ = dmatrices(full_name, data, return_type="dataframe")
+        model = LinearRegression(fit_intercept=False).fit(X_, y_)
+        if use == "AIC":
+            metric[var] = get_AIC(model, X_, y_)
+        elif use == "BIC":
+            metric[var] = get_BIC(model, X_, y_)
+    
+    mini = metric['full']
+    mini_key = 'full'
+    for key, val in metric.items():
+        if val < mini:
+            mini = val
+            mini_key = key
+    
+    if mini_key == "full":
+        kept_vars = data.drop(response, axis=1).columns.tolist()
+        if trace == True:
+            print("Variables in the final model: {}".format(kept_vars))
+        return kept_vars
+    else:
+        if trace == True:
+            print("Remove variable: {} ({}).".format(mini_key, mini))
+        dat = data.drop(mini_key, axis = 1)
+        return backward(dat, response, use=use, trace=trace)
+        
+print(backward(teengamb, "gamble"))
+print(backward(teengamb, "gamble", use = "BIC"))
+
+## (c)
+y_3, X_3a = dmatrices("gamble ~ sex+income+verbal",
+                      teengamb, return_type = "dataframe")
+model_3a = LinearRegression(fit_intercept=False).fit(X_3a, y_3)
+_, X_3b = dmatrices("gamble ~ sex+income",
+                    teengamb, return_type = "dataframe")
+model_3b = LinearRegression(fit_intercept=False).fit(X_3b, y_3)
+print(ANOVA(model_3b, model_3a, X_3b, X_3a, y_3))
+
+## (d)  ######################################################### Not finished
+##############################################################################
+##############################################################################
+_, X_3d = dmatrices("gamble ~ sex * status * income * verbal",
+                    teengamb, return_type="dataframe")
+del X_3d["Intercept"]
+X_3d['gamble'] = teengamb.gamble.tolist()
+
+## (e)
+print(adjusted_R_squared(model_3a, X_3a, y_3))
+print(adjusted_R_squared(model_3b, X_3b, y_3))
+
+
+
+# Question 4
+
+# Question 5
+
+# Question 6
+## (a)
+beta_0, beta_1,beta_2,beta_3,beta_4,beta_5,beta_6,beta_7,beta_8,beta_9,beta_10,sigma = \
+    1,1,1,1,1,0,0,0,0,0,0,2
+signif = ["Intercept", "x_1", "x_2", "x_3", "x_4"]
+not_sig = ["x_5", "x_6", "x_7", "x_8", "x_9", "x_10"]
+np.random.seed(42)
+dat_dict = {"x_1": np.random.uniform(0, 10, 100).tolist(),
+            "x_2": np.random.uniform(0, 10, 100).tolist(),
+            "x_3": np.random.uniform(0, 10, 100).tolist(),
+            "x_4": np.random.uniform(0, 10, 100).tolist(),
+            "x_5": np.random.uniform(0, 10, 100).tolist(),
+            "x_6": np.random.uniform(0, 10, 100).tolist(),
+            "x_7": np.random.uniform(0, 10, 100).tolist(),
+            "x_8": np.random.uniform(0, 10, 100).tolist(),
+            "x_9": np.random.uniform(0, 10, 100).tolist(),
+            "x_10": np.random.uniform(0, 10, 100).tolist()}
+dat = pd.DataFrame(dat_dict)
+dat["y"] = beta_0 + beta_1*dat["x_1"] + beta_2*dat["x_2"] + \
+           beta_3*dat["x_3"] + beta_4*dat["x_4"] + \
+           np.random.normal(0, sigma, 100)
+
+np.random.seed(42)
+fn_aic, fp_aic, fn_bic, fp_bic = [], [], [], []
+for i in range(50):
+    print(i)
+    dat["y"] = beta_0 + beta_1*dat["x_1"] + beta_2*dat["x_2"] + \
+               beta_3*dat["x_3"] + beta_4*dat["x_4"] + \
+               np.random.normal(0, sigma, 100)
+    output = backward(dat, "y", trace=False)
+    output.append("Intercept")
+    fn_aic.append(5-sum([e in signif for e in output]))
+    fp_aic.append(sum([f in output for f in not_sig]))
+    output_2 = backward(dat, "y", use="BIC", trace=False)
+    output_2.append("Intercept")
+    fn_bic.append(5-sum([e2 in signif for e2 in output_2]))
+    fp_bic.append(sum([f2 in output_2 for f2 in not_sig]))
+
+print(np.mean(fn_aic))  
+print(np.mean(fp_aic))
+print(np.mean(fn_bic))
+print(np.mean(fp_bic))
+    
+## (b)
+np.random.seed(42)
+dat_dict = {"x_1": np.random.uniform(0, 10, 100).tolist(),
+            "x_2": np.random.uniform(0, 10, 100).tolist(),
+            "x_3": np.random.uniform(0, 10, 100).tolist(),
+            "x_4": np.random.uniform(0, 10, 100).tolist(),
+            "x_5": np.random.uniform(0, 10, 100).tolist(),
+            "x_6": np.random.uniform(0, 10, 100).tolist(),
+            "x_7": np.random.uniform(0, 10, 100).tolist()}
+dat = pd.DataFrame(dat_dict)
+dat['x_8'] = dat.x_1 + np.random.normal(0, 0.1, 100)
+dat['x_9'] = dat.x_1 + np.random.normal(0, 0.1, 100)
+dat['x_10'] = dat.x_2 + np.random.normal(0, 0.1, 100)
+
+fn_aic, fp_aic, fn_bic, fp_bic = [], [], [], []
+for i in range(50):
+    print(i)
+    dat["y"] = beta_0 + beta_1*dat["x_1"] + beta_2*dat["x_2"] + \
+               beta_3*dat["x_3"] + beta_4*dat["x_4"] + \
+               np.random.normal(0, sigma, 100)
+    output = backward(dat, "y", trace=False)
+    output.append("Intercept")
+    fn_aic.append(5-sum([e in signif for e in output]))
+    fp_aic.append(sum([f in output for f in not_sig]))
+    output_2 = backward(dat, "y", use="BIC", trace=False)
+    output_2.append("Intercept")
+    fn_bic.append(5-sum([e2 in signif for e2 in output_2]))
+    fp_bic.append(sum([f2 in output_2 for f2 in not_sig]))
+
+print(np.mean(fn_aic))  
+print(np.mean(fp_aic))
+print(np.mean(fn_bic))
+print(np.mean(fp_bic))
